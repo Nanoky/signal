30994e07cbc2d22baacf17509b6173865e67f225
nanoky <konancharlesyao@gmail.com>
2022-07-06 13:08:56 +0000

    Location and image got on click

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button } from '@rneui/base';
+import * as ImagePicker from 'expo-image-picker';
+import * as Location from 'expo-location';
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
+
+  const [location, setLocation] = useState(null);
+  const [picture, setPicture] = useState(null);
+
+  const handleSignal = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.error('Permission to access location was denied');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    console.log(location);
+    setLocation(location);
+
+    let cameraPermission = await ImagePicker.requestCameraPermissionsAsync()
+
+    if (cameraPermission.status !== 'granted') {
+      console.error('Permission to access camera was denied');
+      return;
+    }
+
+    let image = await ImagePicker.launchCameraAsync();
+    console.log(image);
+    setPicture(image)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title={"Signaler"} onPress={handleSignal} />
     </View>
   );
 }
